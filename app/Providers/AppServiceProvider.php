7c31863e8966ac5a7375f224c979505f5631de51
A. Gideonse <arno.gideonse@proton.me>
2025-09-26 19:51:31 +0200

    Add AppServiceProvider for settings

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use App\Models\Setting;
+use Illuminate\Support\Facades\Schema;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        if (Schema::hasTable('settings'))
+        {
+            $settings = Setting::all()->pluck('value', 'key')->toArray();
+            config(['settings' => $settings]);
+        }
+
+    }
+}
