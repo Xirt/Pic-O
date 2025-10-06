bed0f0f5f31986619f182eafa8b09d9930216d27
A. Gideonse <arno.gideonse@proton.me>
2025-10-06 22:39:51 +0200

    Added 'create user'-button

--- src/resources/views/partials/section_users.blade.php
+++ src/resources/views/partials/section_users.blade.php
@@ -1,37 +1,51 @@
-<table class="table table-striped table-hover align-middle">
-    <thead>
-        <tr>
-            <th scope="col" class="col-id text-center">#</th>
-            <th scope="col">Name</th>
-            <th scope="col">Email</th>
-            <th scope="col" class="col-role text-center">Role</th>
-            <th scope="col" class="col-actions text-end">&nbsp;</th>
-        </tr>
-    </thead>
-    <tbody>
-        @forelse ($users as $user)
-            <tr>
-                <th scope="row" class="text-center">{{ $user->id }}</th>
-                <td>{{ $user->name }}</td>
-                <td>{{ $user->email }}</td>
-                <td class="text-center">{{ ucfirst($user->role->value) }}</td>
-                <td class="text-end">
-                    <button type="button" class="btn btn-sm btn-primary" data-bs-toggle="offcanvas" data-bs-target="#offcanvas-modify-user" data-user-id="{{ $user->id }}">
-                        <i class="bi bi-pencil me-1"></i> Modify
-                    </button>
 
-                    <button type="button" class="btn btn-sm btn-secondary" onclick="confirmUserDelete({{ $user->id }})">
-                        <i class="bi bi-trash me-1"></i> Delete
-                    </button>
-                </td>
-            </tr>
-        @empty
+<p>Welcome to the administration panel for managing users and their roles. Here you can create, update, and remove user accounts, assign roles, and reset passwords.Use this section to ensure the right people have the right permissions to view your gallery.</p>
+
+
+<div class="mx-lg-5">
+
+    <div class="text-end my-2 mb-3">
+        <button type="button" class="btn btn-sm btn-primary" data-bs-toggle="offcanvas" data-bs-target="#offcanvas-create-user" aria-controls="offcanvas-create-user">
+            <i class="bi bi-person-fill-add me-1"></i> Create user
+        </button>
+    </div>
+
+    <table class="table table-striped table-hover align-middle">
+        <thead>
             <tr>
-                <td colspan="5" class="text-center text-muted">No users found.</td>
+                <th scope="col" class="col-id text-center">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Email</th>
+                <th scope="col" class="col-role text-center">Role</th>
+                <th scope="col" class="col-actions text-end">&nbsp;</th>
             </tr>
-        @endforelse
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            @forelse ($users as $user)
+                <tr>
+                    <th scope="row" class="text-center">{{ $user->id }}</th>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td class="text-center">{{ ucfirst($user->role->value) }}</td>
+                    <td class="text-end">
+                        <button type="button" class="btn btn-sm btn-primary" data-bs-toggle="offcanvas" data-bs-target="#offcanvas-modify-user" data-user-id="{{ $user->id }}">
+                            <i class="bi bi-pencil me-1"></i> Modify
+                        </button>
+
+                        <button type="button" class="btn btn-sm btn-secondary" onclick="confirmUserDelete({{ $user->id }})">
+                            <i class="bi bi-trash me-1"></i> Delete
+                        </button>
+                    </td>
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="5" class="text-center text-muted">No users found.</td>
+                </tr>
+            @endforelse
+        </tbody>
+    </table>
+
+</div>
 
 <div class="offcanvas offcanvas-bottom offcanvas-bottom-sm" tabindex="-1" id="offcanvas-create-user" aria-labelledby="offcanvasCreateUserLabel">
 
