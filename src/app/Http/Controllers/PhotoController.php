1be8a7b9319e8f5865124114f260ef0f8aa1b131
A. Gideonse <arno.gideonse@proton.me>
2025-10-23 23:50:03 +0200

    Redirect to login automatically

--- src/app/Http/Controllers/PhotoController.php
+++ src/app/Http/Controllers/PhotoController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 use App\Models\Photo;
 use App\Services\PhotoService;
 
@@ -11,8 +13,13 @@ class PhotoController extends Controller
      * Show one or more Photos
      * GET /
      */
-    public function index()
+    public function index(Request $request)
     {
+        if (!$request->user())
+        {
+            return redirect()->route('login');
+        }
+
         $this->authorize('viewAny', Photo::class);
 
         return view('pages.timeline');
