1a694d5528d0da1bac09cead9147daed0a337de8
A. Gideonse <arno.gideonse@proton.me>
2025-10-25 20:31:59 +0200

    Modularized PicoView HTML

---
+++ src/resources/views/partials/module-picoview.blade.php
@@ -0,0 +1,57 @@
+<div id="picoView" class="picoview d-none d-flex justify-content-center align-items-center position-fixed top-0 start-0 w-100 h-100 bg-black" aria-hidden="true">
+
+    <div class="bg-light position-absolute top-0 end-0 m-2 p-2 rounded" style="z-index:2; --bs-bg-opacity: .5;">
+
+        <button id="slideshowBtn" class="btn btn-secondary me-1" title="Toggle slideshow">
+            <i class="bi bi-play-fill"></i>
+            <i class="bi bi-stop-fill"></i>
+        </button>
+
+        <button id="closeBtn" class="btn btn-secondary" title="Close">
+            <i class="bi bi-x-lg"></i>
+        </button>
+
+    </div>
+
+    <button id="prevBtn" class="btn btn-secondary btn-swipe slideshow-hide position-absolute top-50 start-0 translate-middle-y m-3" style="z-index:2;"><i class="bi bi-chevron-left"></i></button>
+
+    <div id="picoViewInner" class="picoview-inner w-100 h-100 position-relative overflow-hidden">
+
+        <div class="spinner-overlay d-flex align-items-center justify-content-center d-none" id="spinner">
+            <div class="spinner-border text-secondary" role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
+        </div>
+    </div>
+
+    <button id="nextBtn" class="btn btn-secondary btn-swipe slideshow-hide position-absolute top-50 end-0 translate-middle-y m-3" style="z-index:2;"><i class="bi bi-chevron-right"></i></button>
+
+    <div id="toolbar" class="toolbar slideshow-hide bg-light position-absolute bottom-0 start-50 translate-middle-x d-flex gap-2 m-2 p-2 rounded" style="z-index:2; --bs-bg-opacity: .5;">
+
+        @if($showInfoButton ?? false)
+        <button class="btn btn-secondary action" data-action="photo.info" title="Show details">
+            <i class="bi bi-info-circle-fill"></i>
+        </button>
+        @endif
+
+        @if($showCoverButton ?? false)
+        <button class="btn btn-secondary action" data-action="photo.cover" title="Set as album cover">
+            <i class="bi bi-star-fill"></i>
+        </button>
+        @endif
+
+        @if($showDownloadButton ?? false)
+        <button class="btn btn-secondary action" data-action="photo.download" title="Download original">
+            <i class="bi bi-download"></i>
+        </button>
+        @endif
+
+        @if($showRemoveButton ?? false)
+        <button class="btn btn-secondary action" data-action="photo.remove" title="Remove from album">
+            <i class="bi bi-trash3"></i>
+        </button>
+        @endif
+
+    </div>
+
+</div>
