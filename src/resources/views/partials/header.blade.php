e413cd5f1a356a00e9d077f1a63bf088731fc5e9
A. Gideonse <arno.gideonse@proton.me>
2025-11-16 13:03:56 +0100

    Improved navbar (icons, hover. split)

--- src/resources/views/partials/header.blade.php
+++ src/resources/views/partials/header.blade.php
@@ -1,46 +1,78 @@
 <header class=" sticky-top">
 @auth
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
+
     <div class="container-fluid">
-        <a href="{{ route('home') }}" class="navbar-brand">
+
+        <a href="{{ route('home') }}" class="navbar-brand me-3">
             <img src="{{ asset('favicon.svg') }}" alt="Icon">
         </a>
+
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav me-auto">
-                @auth
 
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('albums.index') }}">Albums</a>
+        <div class="collapse navbar-collapse" id="navbarNav">
+
+            <ul class="navbar-nav me-auto">
+
+                @auth
+                <li class="nav-item me-3">
+                    <a class="nav-link" href="{{ route('home') }}">
+                        <i class="bi bi-calendar-week me-2"></i>
+                        Timeline
+                    </a>
                 </li>
 
-					@if(auth()->check() && auth()->user()->role->value === 'admin')
-					<li class="nav-item">
-						<a class="nav-link" href="{{ route('folders.index') }}">Folders</a>
-					</li>
+                <li class="nav-item me-3">
+                    <a class="nav-link" href="{{ route('albums.index') }}">
+                        <i class="bi bi-images me-2"></i>
+                        Albums
+                    </a>
+                </li>
 
-					<li class="nav-item">
-						<a class="nav-link" href="{{ route('admin.index') }}">Admin</a>
-					</li>
-					@endif
+                @if(auth()->user()->role->value === 'admin')
 
+                <li class="nav-item me-3">
+                    <a class="nav-link" href="{{ route('folders.index') }}">
+                        <i class="bi bi-folder2 me-2"></i>
+                        Folders
+                    </a>
+                </li>
+
+                @endif
                 @endauth
-
             </ul>
+
             <ul class="navbar-nav">
+
+                @if(auth()->user()->role->value === 'admin')
+
+                <li class="nav-item me-2">
+                    <a class="nav-link" href="{{ route('admin.index') }}">
+                        <i class="bi bi-gear me-2 me-lg-0"></i>
+                        <span class="d-lg-none">Administration</span>
+                    </a>
+                </li>
+
+                @endif
+
                 @auth
-
-				<li class="nav-item">
-					<a class="nav-link" href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
-				</li>
-
+                <li class="nav-item me-2">
+                    <a class="nav-link" href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        <i class="bi bi-box-arrow-right me-2 me-lg-0"></i>
+                        <span class="d-lg-none">Logout</span>
+                    </a>
+                </li>
                 @endauth
             </ul>
+
         </div>
+
     </div>
+
 </nav>
+
 @endauth
 
 <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
